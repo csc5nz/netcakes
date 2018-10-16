167f6ef1c5d0f377f1d780d5d41af1b1cabcbc00
nn3un <nn3un@virginia.edu>
2018-10-15 23:23:40 -0400

    Updated state dropdown

--- netcakes/signUp.php
+++ netcakes/signUp.php
@@ -227,65 +227,76 @@ Free for personal and commercial use under the CCA 3.0 license (html5up.net/lice
                 <span class="error"><?php echo $cityErr;?></span>
             </div>
 
+            
             <!-- State -->
             <div class="form-group">
                 <label for="state">State: </label><select class="form-control" name = "state">
-                    <!-- will add more states -->
                     <option value="">----Select State----</option>
-                    <option value="AL" <?php if ($state=="AL") echo "selected";?>>Alabama</option>
-                    <option value="AK" <?php if ($state=="AK") echo "selected";?>>Alaska</option>
-                    <option value="AZ">Arizona</option>
-                    <option value="AR">Arkansas</option>
-                    <option value="CA">California</option>
-                    <option value="CO">Colorado</option>
-                    <option value="CT">Connecticut</option>
-                    <option value="DE">Delaware</option>
-                    <option value="DC">District Of Columbia</option>
-                    <option value="FL">Florida</option>
-                    <option value="GA">Georgia</option>
-                    <option value="HI">Hawaii</option>
-                    <option value="ID">Idaho</option>
-                    <option value="IL">Illinois</option>
-                    <option value="IN">Indiana</option>
-                    <option value="IA">Iowa</option>
-                    <option value="KS">Kansas</option>
-                    <option value="KY">Kentucky</option>
-                    <option value="LA">Louisiana</option>
-                    <option value="ME">Maine</option>
-                    <option value="MD">Maryland</option>
-                    <option value="MA">Massachusetts</option>
-                    <option value="MI">Michigan</option>
-                    <option value="MN">Minnesota</option>
-                    <option value="MS">Mississippi</option>
-                    <option value="MO">Missouri</option>
-                    <option value="MT">Montana</option>
-                    <option value="NE">Nebraska</option>
-                    <option value="NV">Nevada</option>
-                    <option value="NH">New Hampshire</option>
-                    <option value="NJ">New Jersey</option>
-                    <option value="NM">New Mexico</option>
-                    <option value="NY">New York</option>
-                    <option value="NC">North Carolina</option>
-                    <option value="ND">North Dakota</option>
-                    <option value="OH">Ohio</option>
-                    <option value="OK">Oklahoma</option>
-                    <option value="OR">Oregon</option>
-                    <option value="PA">Pennsylvania</option>
-                    <option value="RI">Rhode Island</option>
-                    <option value="SC">South Carolina</option>
-                    <option value="SD">South Dakota</option>
-                    <option value="TN">Tennessee</option>
-                    <option value="TX">Texas</option>
-                    <option value="UT">Utah</option>
-                    <option value="VT">Vermont</option>
-                    <option value="VA">Virginia</option>
-                    <option value="WA">Washington</option>
-                    <option value="WV">West Virginia</option>
-                    <option value="WI">Wisconsin</option>
-                    <option value="WY">Wyoming</option>
+                    <!-- Generate state option html tags dynamically and select selected state -->
+                    <?php
+                    $state_array = array(
+                        'AL'=>'Alabama',
+                        'AK'=>'Alaska',
+                        'AZ'=>'Arizona',
+                        'AR'=>'Arkansas',
+                        'CA'=>'California',
+                        'CO'=>'Colorado',
+                        'CT'=>'Connecticut',
+                        'DE'=>'Delaware',
+                        'DC'=>'District of Columbia',
+                        'FL'=>'Florida',
+                        'GA'=>'Georgia',
+                        'HI'=>'Hawaii',
+                        'ID'=>'Idaho',
+                        'IL'=>'Illinois',
+                        'IN'=>'Indiana',
+                        'IA'=>'Iowa',
+                        'KS'=>'Kansas',
+                        'KY'=>'Kentucky',
+                        'LA'=>'Louisiana',
+                        'ME'=>'Maine',
+                        'MD'=>'Maryland',
+                        'MA'=>'Massachusetts',
+                        'MI'=>'Michigan',
+                        'MN'=>'Minnesota',
+                        'MS'=>'Mississippi',
+                        'MO'=>'Missouri',
+                        'MT'=>'Montana',
+                        'NE'=>'Nebraska',
+                        'NV'=>'Nevada',
+                        'NH'=>'New Hampshire',
+                        'NJ'=>'New Jersey',
+                        'NM'=>'New Mexico',
+                        'NY'=>'New York',
+                        'NC'=>'North Carolina',
+                        'ND'=>'North Dakota',
+                        'OH'=>'Ohio',
+                        'OK'=>'Oklahoma',
+                        'OR'=>'Oregon',
+                        'PA'=>'Pennsylvania',
+                        'RI'=>'Rhode Island',
+                        'SC'=>'South Carolina',
+                        'SD'=>'South Dakota',
+                        'TN'=>'Tennessee',
+                        'TX'=>'Texas',
+                        'UT'=>'Utah',
+                        'VT'=>'Vermont',
+                        'VA'=>'Virginia',
+                        'WA'=>'Washington',
+                        'WV'=>'West Virginia',
+                        'WI'=>'Wisconsin',
+                        'WY'=>'Wyoming',
+                    );
+                    foreach($state_array as $state_abrv => $state_name) {
+                        $selected = "";
+                        if ($state_abrv == $state) $selected = 'selected';
+                        echo ('<option value="' . $state_abrv . '" ' . $selected . '>' . $state_name . '</option>');
+                    }
+                    ?>
                 </select>
-                <span class="error"><?php echo $stateErr;?></span>
+                <span class="error"style="color:red"><?php echo $stateErr;?></span>
             </div>
+
 
             <!-- zip code -->
             <div class="form-group">
