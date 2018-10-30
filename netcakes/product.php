fc9c433fe5af804a3c65d91656aa8af629a87961
nn3un <nn3un@virginia.edu>
2018-10-30 16:21:05 -0400

    Change to bitpay payment method

--- netcakes/product.php
+++ netcakes/product.php
@@ -52,87 +52,86 @@
 								<p>$10.00</p>
 							</header>
 							<form action="https://test.bitpay.com/checkout" method="post" >
-							  <input type="hidden" name="action" value="cartAdd" />
-							  <input type="hidden" name="data" value="VJxorWWo9vChUjez8TFUkW" />
-							  <input type="image" src="https://test.bitpay.com/cdn/en_US/bp-btn-pay-currencies.svg" name="submit" style="width: 210px;" alt="BitPay, the easy way to pay with bitcoins." >
+								<input type="hidden" name="action" value="cartAdd" />
+								<input type="hidden" name="data" value="EXKjPnaCXdY2dfeD7Fs7yd" />
+								<input type="image" src="https://test.bitpay.com/cdn/en_US/bp-btn-pay-currencies.svg" name="submit" style="width: 210px;" alt="BitPay, the easy way to pay with bitcoins." >
+							</form>
+						</section>
+					</div>
+
+					<div class="col-4 col-12-medium"
+					<!-- Box -->
+					<section class="box feature">
+						<a href="#" class="image featured"><img src="images/Cupcakes.jpg " alt="" /></a>
+						<div class="inner">
+							<header>
+								<h2>Dozen Cupcakes</h2>
+								<p>$3.00</p>
+							</header>
+							<form action="https://test.bitpay.com/checkout" method="post" >
+								<input type="hidden" name="action" value="cartAdd" />
+								<input type="hidden" name="data" value="41NGL7cWLGebMJpuJC418n" />
+								<input type="image" src="https://test.bitpay.com/cdn/en_US/bp-btn-pay-currencies.svg" name="submit" style="width: 210px;" alt="BitPay, the easy way to pay with bitcoins." >
 							</form>
 						</div>
 					</section>
 				</div>
-
-				<div class="col-4 col-12-medium"
-				<!-- Box -->
-				<section class="box feature">
-					<a href="#" class="image featured"><img src="images/Cupcakes.jpg " alt="" /></a>
-					<div class="inner">
-						<header>
-							<h2>Dozen Cupcakes</h2>
-							<p>$3.00</p>
-						</header>
-						<form action="https://test.bitpay.com/checkout" method="post" >
-						  <input type="hidden" name="action" value="cartAdd" />
-						  <input type="hidden" name="data" value="VJxorWWo9vChUjez8TFUkW" />
-						  <input type="image" src="https://test.bitpay.com/cdn/en_US/bp-btn-pay-currencies.svg" name="submit" style="width: 210px;" alt="BitPay, the easy way to pay with bitcoins." >
-						</form>
-					</div>
-				</section>
 			</div>
 		</div>
 	</div>
-</div>
 
 
-<!-- Footer -->
-<div id="footer-wrapper">
-	<footer id="footer" class="container">
-		<div class="row">
+	<!-- Footer -->
+	<div id="footer-wrapper">
+		<footer id="footer" class="container">
+			<div class="row">
+				<div class="col-3 col-6-medium col-12-small">
+
+					<!-- Links -->
+					<section class="widget links">
+						<h3>Directory</h3>
+						<ul class="style2">
+							<li><a href="index.php">Welcome</a></li>
+							<li><a href="about-us.php">About Us</a></li>
+						</li>
+						<li><a href="contactUs.php">Contact Us</a></li>
+						<li class="current"><a href="signUp.php">Sign Up</a></li>
+						<li><a href="login.php">Log In</a></li>
+					</ul>
+				</section>
+
+			</div>
+
 			<div class="col-3 col-6-medium col-12-small">
 
-				<!-- Links -->
-				<section class="widget links">
-					<h3>Directory</h3>
-					<ul class="style2">
-						<li><a href="index.php">Welcome</a></li>
-						<li><a href="about-us.php">About Us</a></li>
-					</li>
-					<li><a href="contactUs.php">Contact Us</a></li>
-					<li class="current"><a href="signUp.php">Sign Up</a></li>
-					<li><a href="login.php">Log In</a></li>
-				</ul>
-			</section>
+				<!-- Contact -->
+				<section class="widget contact last">
+					<h3>Contact Us</h3>
+					<ul>
+						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+						<li><a href="#" class="icon fa-pinterest"><span class="label">Pinterest</span></a></li>
+					</ul>
+					<p>1234 Fictional Road<br />
+						Charlottesville, VA 22904<br />
+					(800) 555-0000</p>
+				</section>
 
-		</div>
-
-		<div class="col-3 col-6-medium col-12-small">
-
-			<!-- Contact -->
-			<section class="widget contact last">
-				<h3>Contact Us</h3>
-				<ul>
-					<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-					<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-					<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-					<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
-					<li><a href="#" class="icon fa-pinterest"><span class="label">Pinterest</span></a></li>
-				</ul>
-				<p>1234 Fictional Road<br />
-					Charlottesville, VA 22904<br />
-				(800) 555-0000</p>
-			</section>
-
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-12">
-			<div id="copyright">
-				<ul class="menu">
-					<li>
-					&copy; Netcakes. All rights reserved</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-				</ul>
 			</div>
 		</div>
-	</div>
-</footer>
+		<div class="row">
+			<div class="col-12">
+				<div id="copyright">
+					<ul class="menu">
+						<li>
+						&copy; Netcakes. All rights reserved</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</footer>
 </div>
 
 </div>
